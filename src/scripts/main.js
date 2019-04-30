bf46397f90d300344190ad184fe89a0216d90d83
Sean Glavin <sean@Seans-MacBook-Pro.local>
2019-04-30 15:05:50 -0500

    artists sign and then add the labelid to the api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,24 @@ const artistContainer = document.querySelector("#artistContainer")
 
 const newsContainer = document.querySelector("#newsContainer")
 
+const newsPost = document.querySelector("#newsContainer")
+
+let counter = 1
+API.getArtists()
+.then(elements => {
+    elements.forEach(artist => {
+            let name = artist.name
+            let age = artist.age
+            let genreId = artist.genresId
+            API.getGenres(genreId)
+                .then(response => {
+                    let artistGenre = response[0].name
+                    let domPush = makeArtistHTML(name, age, artistGenre, counter, genreId)
+                    pushArtistToDOM(domPush)
+                    counter++
+                })
+                .then(response => {
+                    createArtistButtonListener()
+                })
+            })
+    })
--- src/scripts/pushToDom.js
+++ src/scripts/pushToDom.js
@@ -1,8 +1,8 @@
-function makeArtistHTML (name, age, genre, id) {
-    return `<div class="artists">
+function makeArtistHTML (name, age, genre, id, genreId) {
+    return `<div class="artists" id="container${id}">
         <h1>${name}</h1>
         <h2>Age: ${age}</h2>
-        <h2>Genre: ${genre}</h2>
+        <h3 id="genre${genreId}">Genre: ${genre}</h3>
         <button class="artistBtn" id="artist${id}">SIGN ARTIST</button>
     </div>`
 }
@@ -11,43 +11,59 @@ function pushArtistToDOM(toBeAdded) {
     artistContainer.innerHTML += toBeAdded
 }
 
+function makeLabelAnnouncement(label, name) {
+    return `<div class="news">
+            <h1>BREAKING NEWS</h1>
+            <h2>${label} has signed ${name}!!</h2>
+            <button class="newsBtn">GO BACK</button>
+            </div>`
+}
+
+function pushNewsToDOM(toBeAdded) {
+    newsContainer.innerHTML = toBeAdded
+}
+
+function createNewsListener() {
+    let newsBtn = document.querySelector(".newsBtn")
+    newsBtn.addEventListener("click", () => {
+        newsPost.style.display = "none";
+    })
+}
+
+function createPostObj (labelId) {
+    return {
+        labelsId: labelId
+    };
+}
+
+
+
 function createArtistButtonListener () {
     let buttons = document.querySelectorAll(".artistBtn")
     buttons.forEach(button => {
         let idNumber = button.id.slice(6)
+        let artistGenreId = document.querySelector(`#container${idNumber} > h3`).id.slice(5)
         button.addEventListener("click", () => {
             console.log(`you clicked artist #${idNumber}`)
-            API.getlabels_genres(genreId)
+            API.getLabelGenre(artistGenreId)
                 .then(reply => {
-                    let labelId = reply.labelId
+                    let labelId = reply[0].labelsId
                     API.getLabels(labelId)
                     .then(labels => {
-                        let label = labels.name
-                        console.log(label)
+                        let label = labels[0].name
+                        let name = document.querySelector(`#container${idNumber} > h1`).textContent
+                        let news = makeLabelAnnouncement(label, name)
+                        pushNewsToDOM(news)
+                        newsPost.style.display = "flex"
+                        createNewsListener()
+                        let postOBJ = createPostObj(labelId)
+                        API.postArtistLabel(idNumber, postOBJ)
+
                     })
                 })
-        })
+            })
     })
 }
 
 
 
-let counter = 1
-API.getArtists()
-.then(elements => {
-    elements.forEach(artist => {
-            let name = artist.name
-            let age = artist.age
-            let genreId = artist.genresId
-            API.getGenres(genreId)
-                .then(response => {
-                    let artistGenre = response[0].name
-                    let domPush = makeArtistHTML(name, age, artistGenre, counter)
-                    pushArtistToDOM(domPush)
-                    counter++
-                })
-                .then(response => {
-                    createArtistButtonListener()
-                })
-            })
-    })
